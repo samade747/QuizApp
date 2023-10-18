2b11438c7e2a06c5e935cf3e052055b5e98d7a86
samade747 <samad.e747@gmail.com>
2023-10-18 13:31:21 +0500

    assa

--- app.js
+++ app.js
@@ -164,11 +164,11 @@ var score = 0;
 
 // timer 
 var min = 1;
-var sec = 59;
+var sec = 25;
 var timer = document.getElementById(`timer`);
 var interval = setInterval(function(){
     timer.innerHTML = `${min} : ${sec}`
-    sec--
+    sec--;
 },1000)
 
 
